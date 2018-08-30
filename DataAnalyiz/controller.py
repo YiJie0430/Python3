2e146780e20654598c9630ae792c67c06654a049
Yijie0430 <sogo0430tw@gmail.com>
2018-08-30 17:11:03 +0800

    controller
    function adding

--- DataAnalyiz/controller.py
+++ DataAnalyiz/controller.py
@@ -2,14 +2,24 @@
 # -*- coding: utf-8 -*-
 import os
 import pathlib
+import re
 from model import dirFunc
+from functools import wraps
 
 
 class analizyFun:
     def __init__(self, *args):
         self.fileFolder = list()
+        self.parseResult = list()
         self.fileDic = dict()
         self.rawpath = args[0]
+        self.regex = [r'Test Result.*', r'MAC Address.*',
+                      r'^Start\sTime.*', r'Station.\s.*?\s', r'dut_id.*']
+        self.regexDic = {'TestResult': r'Test Result.*',
+                         'MAC': r'MAC Address.*',
+                         'StartTime': r'Start\sTime.*(................:.......)',
+                         'StationID': r'Station.\s.*?\s',
+                         'DutID': r'dut_id.*'}
 
     def fileCollect(self):
         dirList = dirFunc().walkDir(self.rawpath)
@@ -20,12 +30,21 @@ class analizyFun:
             # self.fileFolder.append(fileList)
         return self.fileDic
 
-    def logParse(self, log):
-        with open(log, 'r') as f:
-            read_data = f.read()
-            # print (read_data)
-        f.close()
-        return read_data
+    def logParse(self, data):
+        for regex in self.regexDic.keys():
+            pattern = re.compile(self.regexDic[regex], re.MULTILINE | re.IGNORECASE)
+            parsing = (pattern.findall(data))
+            print ('parsing {}: {}'.format(regex, parsing))
+            if parsing:
+                if regex is 'StartTime':
+                    parse = parsing[0]
+                else:
+                    parse = parsing[0].split(':')[-1].split('\n')[0].strip()
+                print (parse)
+            else:
+                parse = 'NONE'
+            self.parseResult.append(parse)
+        return self.parseResult
 
     def fileAnalizy(self, files):
         anaDir = dirFunc().createAnalizydir(list(files.keys()))[1][0]
@@ -33,8 +52,14 @@ class analizyFun:
         for station in files:
             for log in files[station]:
                 logPath = self.rawpath + '\\' + station + '\\' + log
-                a = self.logParse(logPath)
-                print (a)
+                with open(logPath, 'r') as f:
+                    content = f.read()
+                f.close()
+                a = self.logParse(content)
+        print (a)
+        print (len(a))
+
+
 
 
 def main():
